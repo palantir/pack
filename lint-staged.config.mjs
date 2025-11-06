0b22f5584d815133323210764819120275bd5711
zerovox <933064+zerovox@users.noreply.github.com>
2025-11-06 14:07:34 -0700

    cspell on modified files

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,7 @@
 export default {
-  "**/*.{js,ts,jsx,tsx,json,md}": () => "turbo lint:fix --force --filter=[HEAD^1]",
+  "**/*.{js,ts,jsx,tsx,json,md}": (filenames) => [
+    "turbo lint:fix --force --filter=[HEAD^1]",
+    `cspell --quiet --no-must-find-files --file ${filenames.join(" ")}`,
+  ],
   "**/{package.json,tsconfig.json,vitest.config.mjs,.monorepolint.config.mjs}": () => "turbo mrl:check --force",
 }
