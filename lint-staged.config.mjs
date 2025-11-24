e1df2ee98893aa88567cb9e9f512d48344e1bf4f
Alex de la Mare <alexdlm@alexdlm.org>
2025-11-24 14:33:45 +1100

    Faster lintstaged

--- lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  "**/*.{js,ts,jsx,tsx,json,md}": () => [
-    "turbo lint:fix --force --filter=[HEAD^1]",
-  ],
-  "*": (filenames) => `cspell --quiet --no-must-find-files ${filenames.join(" ")}`,
-  "**/{package.json,tsconfig.json,vitest.config.mjs,.monorepolint.config.mjs}": () => "turbo mrl:check --force",
-}
