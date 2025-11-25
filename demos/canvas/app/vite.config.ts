6098bed8568895eae9b6dadab9357c8776044cb4
Alex de la Mare <alexdlm@alexdlm.org>
2025-11-26 03:15:17 +1100

    Add canvas demo boilerplate (#83)

---
+++ demos/canvas/app/vite.config.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === "development" ? process.env.DEV_SERVER_BASE_PATH : undefined,
+  server: {
+    host: process.env.DEV_SERVER_HOST,
+    port: Number(process.env.DEV_SERVER_PORT ?? 5173),
+    proxy: {
+      "^(/api/v2)": {
+        target: process.env.VITE_FOUNDRY_API_URL,
+        changeOrigin: true,
+        secure: true,
+        ws: true,
+      },
+    },
+  },
+});
---
+++ demos/canvas/schema/src/schema.mjs
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as S from "@palantir/pack.schema";
+
+// @ts-check
+
+const SHAPE_COMMON = {
+  bottom: S.Double,
+  left: S.Double,
+  right: S.Double,
+  top: S.Double,
+
+  color: S.Optional(S.String),
+};
+
+const migration000 = S.defineMigration({}, () => {
+  const ShapeCircle = S.defineRecord("ShapeCircle", {
+    docs: "A circle.",
+    fields: SHAPE_COMMON,
+  });
+
+  const ShapeBox = S.defineRecord("ShapeBox", {
+    docs: "A box.",
+    fields: SHAPE_COMMON,
+  });
+
+  const NodeShape = S.defineUnion("NodeShape", {
+    docs: "The shape of a node.",
+    variants: {
+      "box": ShapeBox,
+      "circle": ShapeCircle,
+    },
+  });
+
+  return {
+    NodeShape,
+    ShapeBox,
+    ShapeCircle,
+  };
+});
+
+export default migration000;
