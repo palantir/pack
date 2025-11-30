91a7983d767eb84893f20d058f1bca965202b56b
Alex de la Mare <alexdlm@alexdlm.org>
2025-12-01 09:59:34 +1100

    Add demo service for offline dev

---
+++ packages/state/demo/src/PresenceManager.ts
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type {
+  ActivityEvent,
+  DocumentId,
+  PresenceEvent,
+  PresenceEventDataType,
+  UserId,
+} from "@palantir/pack.document-schema.model-types";
+
+const HEARTBEAT_INTERVAL_MS = 5000;
+const STALE_CLIENT_TIMEOUT_MS = 15000;
+
+type PresenceChannelMessage =
+  | { readonly type: "heartbeat"; readonly userId: string; readonly timestamp: number }
+  | { readonly type: "activity"; readonly userId: string; readonly event: ActivityEvent };
+
+export class PresenceManager {
+  private readonly channel: BroadcastChannel;
+  private readonly userId: UserId;
+  private readonly activeClients = new Map<UserId, number>();
+  private heartbeatInterval?: number;
+  private staleCheckInterval?: number;
+  private presenceCallbacks = new Set<(event: PresenceEvent) => void>();
+  private activityCallbacks = new Set<(event: ActivityEvent) => void>();
+
+  constructor(documentId: DocumentId, userId: string) {
+    this.userId = userId as UserId;
+    this.channel = new BroadcastChannel(`pack-demo-presence-${documentId}`);
+
+    this.channel.onmessage = event => {
+      this.handleMessage(event.data as PresenceChannelMessage);
+    };
+
+    this.startHeartbeat();
+    this.startStaleCheck();
+  }
+
+  onPresence(callback: (event: PresenceEvent) => void): () => void {
+    this.presenceCallbacks.add(callback);
+    return () => {
+      this.presenceCallbacks.delete(callback);
+    };
+  }
+
+  onActivity(callback: (event: ActivityEvent) => void): () => void {
+    this.activityCallbacks.add(callback);
+    return () => {
+      this.activityCallbacks.delete(callback);
+    };
+  }
+
+  broadcastActivity(event: ActivityEvent): void {
+    const message: PresenceChannelMessage = {
+      event,
+      type: "activity",
+      userId: this.userId,
+    };
+    this.channel.postMessage(message);
+  }
+
+  dispose(): void {
+    if (this.heartbeatInterval != null) {
+      clearInterval(this.heartbeatInterval);
+      this.heartbeatInterval = undefined;
+    }
+
+    if (this.staleCheckInterval != null) {
+      clearInterval(this.staleCheckInterval);
+      this.staleCheckInterval = undefined;
+    }
+
+    this.channel.close();
+    this.presenceCallbacks.clear();
+    this.activityCallbacks.clear();
+    this.activeClients.clear();
+  }
+
+  private startHeartbeat(): void {
+    const sendHeartbeat = () => {
+      const message: PresenceChannelMessage = {
+        timestamp: Date.now(),
+        type: "heartbeat",
+        userId: this.userId,
+      };
+      this.channel.postMessage(message);
+    };
+
+    sendHeartbeat();
+    this.heartbeatInterval = setInterval(sendHeartbeat, HEARTBEAT_INTERVAL_MS) as unknown as number;
+  }
+
+  private startStaleCheck(): void {
+    this.staleCheckInterval = setInterval(() => {
+      this.checkStaleClients();
+    }, HEARTBEAT_INTERVAL_MS) as unknown as number;
+  }
+
+  private handleMessage(message: PresenceChannelMessage): void {
+    switch (message.type) {
+      case "heartbeat":
+        this.handleHeartbeat(message.userId, message.timestamp);
+        break;
+      case "activity":
+        this.handleActivity(message.event);
+        break;
+    }
+  }
+
+  private handleHeartbeat(userId: UserId, timestamp: number): void {
+    const wasActive = this.activeClients.has(userId);
+    this.activeClients.set(userId, timestamp);
+
+    if (!wasActive) {
+      const event: PresenceEvent = {
+        eventData: {
+          type: "presenceArrived" as typeof PresenceEventDataType.ARRIVED,
+        },
+        userId,
+      };
+      this.emitPresenceEvent(event);
+    }
+  }
+
+  private handleActivity(event: ActivityEvent): void {
+    for (const callback of this.activityCallbacks) {
+      callback(event);
+    }
+  }
+
+  private checkStaleClients(): void {
+    const now = Date.now();
+    const staleClients: UserId[] = [];
+
+    for (const [userId, lastSeen] of this.activeClients.entries()) {
+      if (now - lastSeen > STALE_CLIENT_TIMEOUT_MS) {
+        staleClients.push(userId);
+      }
+    }
+
+    for (const userId of staleClients) {
+      this.activeClients.delete(userId);
+      const event: PresenceEvent = {
+        eventData: {
+          type: "presenceDeparted" as typeof PresenceEventDataType.DEPARTED,
+        },
+        userId,
+      };
+      this.emitPresenceEvent(event);
+    }
+  }
+
+  private emitPresenceEvent(event: PresenceEvent): void {
+    for (const callback of this.presenceCallbacks) {
+      callback(event);
+    }
+  }
+}
