0b3799ea1c80032efc395ac90e662ec36f263c6b
ts <933064+zerovox@users.noreply.github.com>
2026-01-30 13:04:34 -0700

    Add in extra steps to release commit script to regenerate files with versions in the header. (#136)

--- scripts/createReleaseCommit.sh
+++ scripts/createReleaseCommit.sh
@@ -11,3 +11,14 @@ pnpm exec turbo transpile --filter "./packages/monorepo/release"
 # Run version bump and create commit on current branch with version digest
 node ./packages/monorepo/release/build/esm/index.js \
   --mode version-commit
+
+# Run build and test to regenerate files with new versions (snapshots, demo files, etc.)
+pnpm build
+pnpm test -- --update
+
+# Add empty changeset to pass CI checks
+pnpm exec changeset add --empty
+
+# Amend the release commit with the regenerated files and empty changeset
+git add -A
+git commit --amend --no-edit
