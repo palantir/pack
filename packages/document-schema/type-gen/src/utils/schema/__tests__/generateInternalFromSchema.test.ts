8c0ba767d8177562081d63315a5895d5d32c4802
zerovox <933064+zerovox@users.noreply.github.com>
2026-04-23 17:27:12 -0600

    Add internal generators to the sdk-gen command

---
+++ packages/document-schema/type-gen/src/utils/schema/__tests__/generateInternalFromSchema.test.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import path from "path";
+import { describe, expect, it } from "vitest";
+import { generateInternalFromSchema } from "../generateInternalFromSchema.js";
+import { singleVersionSchema, twoVersionFieldRemovalSchema } from "./fixtures.js";
+import { formatInternalTypesSnapshot } from "./snapshotUtils.js";
+
+describe("generateInternalFromSchema", () => {
+  const snapshotDir = path.join(__dirname, "__snapshots__", "generateInternalFromSchema");
+
+  it("single-version schema", async () => {
+    const result = generateInternalFromSchema(singleVersionSchema);
+    await expect(await formatInternalTypesSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "single-version.snap"),
+    );
+  });
+
+  it("two-version field removal", async () => {
+    const result = generateInternalFromSchema(twoVersionFieldRemovalSchema);
+    await expect(await formatInternalTypesSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "two-version-field-removal.snap"),
+    );
+  });
+});
---
+++ packages/document-schema/type-gen/src/utils/schema/__tests__/generateVersionedZodFromSchema.test.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import path from "path";
+import { describe, expect, it } from "vitest";
+import { generateVersionedZodFromSchema } from "../generateVersionedZodFromSchema.js";
+import {
+  singleVersionSchema,
+  twoVersionAdditiveSchema,
+  twoVersionFieldRemovalSchema,
+  unionTypesSchema,
+} from "./fixtures.js";
+import { formatVersionedZodSnapshot } from "./snapshotUtils.js";
+
+describe("generateVersionedZodFromSchema", () => {
+  const snapshotDir = path.join(__dirname, "__snapshots__", "generateVersionedZodFromSchema");
+
+  it("single-version schema", async () => {
+    const result = generateVersionedZodFromSchema(singleVersionSchema);
+    await expect(await formatVersionedZodSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "single-version.snap"),
+    );
+  });
+
+  it("two-version additive change", async () => {
+    const result = generateVersionedZodFromSchema(twoVersionAdditiveSchema, 1);
+    await expect(await formatVersionedZodSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "two-version-additive.snap"),
+    );
+  });
+
+  it("two-version field removal", async () => {
+    const result = generateVersionedZodFromSchema(twoVersionFieldRemovalSchema, 1);
+    await expect(await formatVersionedZodSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "two-version-field-removal.snap"),
+    );
+  });
+
+  it("schema with union types", async () => {
+    const result = generateVersionedZodFromSchema(unionTypesSchema);
+    await expect(await formatVersionedZodSnapshot(result)).toMatchFileSnapshot(
+      path.join(snapshotDir, "union-types.snap"),
+    );
+  });
+
+  it("respects minSupportedVersion default (latest only)", () => {
+    const result = generateVersionedZodFromSchema(twoVersionAdditiveSchema);
+    expect(result.zodSchemas.size).toBe(1);
+    expect(result.zodSchemas.has(2)).toBe(true);
+    expect(result.zodSchemas.has(1)).toBe(false);
+  });
+});
