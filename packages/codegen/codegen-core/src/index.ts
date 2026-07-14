cc1d9ae399fb807d9b0d9563628d040ef02727d0
zerovox <933064+zerovox@users.noreply.github.com>
2026-07-14 11:28:13 -0700

    Extract @palantir/pack.codegen.core from sdkgen

--- packages/codegen/codegen-core/src/index.ts
+++ packages/codegen/codegen-core/src/index.ts
@@ -14,15 +14,16 @@
  * limitations under the License.
  */
 
+export { ContextBuilder } from "./core/contextBuilder.js";
+export { Generator } from "./core/generator.js";
+export { SchemaParser } from "./core/schemaParser.js";
+export { TemplateLoader } from "./core/templateLoader.js";
 export {
-  ContextBuilder,
-  createTemplateUtils,
-  Generator,
-  Logger,
-  promptUser,
-  SchemaParser,
-  TemplateLoader,
-} from "@palantir/pack.codegen.core";
-export type * from "@palantir/pack.codegen.core";
-export { cli } from "./cli.js";
-export { createCommand } from "./commands/create.js";
+  createProject,
+  type CreateProjectMessaging,
+  type CreateProjectOptions,
+} from "./createProject.js";
+export type * from "./types/index.js";
+export { Logger } from "./utils/logger.js";
+export { promptUser } from "./utils/prompts.js";
+export { createTemplateUtils } from "./utils/templateUtils.js";
